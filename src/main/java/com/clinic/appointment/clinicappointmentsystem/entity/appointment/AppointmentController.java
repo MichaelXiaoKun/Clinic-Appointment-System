ccc9fb5e71f05e89d94bc431e61b2b8138bcf765
Edmure <edmure.windsor@outlook.com>
2023-05-11 01:36:18 -0400

    Edmure

--- src/main/java/com/clinic/appointment/clinicappointmentsystem/entity/appointment/AppointmentController.java
+++ src/main/java/com/clinic/appointment/clinicappointmentsystem/entity/appointment/AppointmentController.java
@@ -1,24 +1,19 @@
 package com.clinic.appointment.clinicappointmentsystem.entity.appointment;
 
+import com.clinic.appointment.clinicappointmentsystem.entity.account.AccountService;
+import com.clinic.appointment.clinicappointmentsystem.entity.account.config.JwtService;
 import com.clinic.appointment.clinicappointmentsystem.entity.doctor.DoctorService;
 import com.clinic.appointment.clinicappointmentsystem.entity.patient.PatientService;
 import com.clinic.appointment.clinicappointmentsystem.exception.exceptionClass.AppointmentDateException;
 import com.clinic.appointment.clinicappointmentsystem.exception.exceptionClass.AppointmentIdNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
-import java.util.List;
-
 import java.time.LocalDate;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
-
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -27,73 +22,227 @@ import static org.springframework.http.HttpStatus.OK;
 public class AppointmentController {
 
     private final AppointmentService appointmentService;
+    private final AccountService accountService;
     private final DoctorService doctorService;
     private final PatientService patientService;
+    private final JwtService jwtService;
 
     @Autowired
     public AppointmentController(AppointmentService appointmentService,
-                                 DoctorService doctorService,
-                                 PatientService patientService) {
+                                 AccountService accountService, DoctorService doctorService,
+                                 PatientService patientService,
+                                 JwtService jwtService) {
 
         this.appointmentService = appointmentService;
+        this.accountService = accountService;
         this.doctorService = doctorService;
         this.patientService = patientService;
+        this.jwtService = jwtService;
     }
 
-    @PostMapping("/make")
-    public ResponseEntity<AppointmentResponse> makeAppointment(@RequestBody AppointmentRequest request) throws AppointmentDateException {
+     /***
+     Creating a REST API: "POST http://localhost:8080/api/account/appointment/patient/patientView/make"
+     Schedule an appointment for logged-in patients
+     Passing in JSON (date should be in "yyyy-MM-dd HH:mm:ss"):
+      {
+          "appointmentTitle": ...,
+          "patientName": ...,
+          "doctorName": ...,
+          "description": ...,
+          "startTime": ...,
+          "endTime": ...
+      }
+     */
+
+    /**
+     * Creating a REST API: "POST http://localhost:8080/api/account/appointment/patient/patientView/make"
+     * Schedule an appointment for logged-in patients
+     * Passing in JSON (date should be in "yyyy-MM-dd HH:mm:ss"):
+     * {
+     *     "appointmentTitle": ...,
+     *     "doctorName": ...,
+     *     "description": ...,
+     *     "startTime": ...,
+     *     "endTime": ...
+     * }
+     *
+     * In Postman, select POST as the method, input the URL, and set the request body to raw JSON.
+     * Authorization header should be set with a valid JWT token for a patient account.
+     */
+    @PostMapping("/patient/patientView/make")
+    public ResponseEntity<AppointmentResponse> makeAppointment(@RequestHeader("Authorization") String authHeader, @RequestBody AppointmentRequest request) throws AppointmentDateException {
+        String jwtToken = authHeader.substring(7);
+        String username = this.jwtService.extractUsername(jwtToken);
+        request.setPatientName(username);
         return ResponseEntity.ok(appointmentService.makeAppointment(request));
     }
 
-    @DeleteMapping("/cancel")
-    public ResponseEntity<AppointmentResponse> cancelAppointment(@RequestBody AppointmentRequest request) throws AppointmentDateException {
+
+    /**
+     * Creating a REST API: "DELETE http://localhost:8080/api/account/appointment/patient/patientView/cancel/{appointmentId}"
+     * Cancel an existing appointment for logged-in patients
+     * An appointmentId needs to be passed in the URL as a path variable.
+     *
+     * In Postman, select DELETE as the method, input the URL, replacing {appointmentId} with a valid appointment ID.
+     * Authorization header should be set with a valid JWT token for a patient account.
+     */
+    @DeleteMapping("/patient/patientView/cancel")
+    public ResponseEntity<AppointmentResponse> cancelAppointment(@RequestHeader("Authorization") String authHeader, @RequestBody AppointmentRequest request) throws AppointmentDateException {
+        String jwtToken = authHeader.substring(7);
+        String username = this.jwtService.extractUsername(jwtToken);
+        request.setPatientName(username);
         return ResponseEntity.ok(appointmentService.cancelAppointment(request));
     }
 
-    @GetMapping("/all")
-    public ResponseEntity<List<AppointmentEntity>> getAllAppointments() {
-        return new ResponseEntity<>(appointmentService.getAllAppointments(), OK);
+
+    /**
+     * Creating a REST API: "GET http://localhost:8080/api/account/appointment/allView/myAppointments"
+     * Get all appointments for the logged-in user (either patient or doctor).
+     *
+     * In Postman, select GET as the method, input the URL.
+     * Authorization header should be set with a valid JWT token for a patient or a doctor account.
+     */
+    @GetMapping("/allView/myAppointments")
+    public ResponseEntity<List<AppointmentEntity>> getAllAppointments(@RequestHeader("Authorization") String authHeader) {
+        String jwtToken = authHeader.substring(7);
+        String username = this.jwtService.extractUsername(jwtToken);
+        String type = accountService.getAccountTypeByUsername(username);
+
+        List<AppointmentEntity> appts;
+        if (type.equals("PATIENT")) {
+            appts = appointmentService.getAppointmentsByPatientUsername(username);
+        } else {
+            appts = appointmentService.getAppointmentsByDoctorUsername(username);
+        }
+
+        return new ResponseEntity<>(appts, OK);
     }
 
-    @GetMapping("/{apptID}")
+
+    /**
+     * Creating a REST API: "GET http://localhost:8080/api/account/appointment/doctor/doctorView/{apptID}"
+     * Get specific appointment details by appointment ID.
+     * An appointmentId needs to be passed in the URL as a path variable.
+     *
+     * In Postman, select GET as the method, input the URL, replacing {apptID} with a valid appointment ID.
+     * Authorization header should be set with a valid JWT token for a doctor account.
+     */
+    @GetMapping("/doctor/doctorView/{apptID}")
     public ResponseEntity<AppointmentEntity> getAppointmentByID(@PathVariable("apptID") int apptID)
             throws AppointmentIdNotFoundException {
         return new ResponseEntity<>(appointmentService.getAppointmentByID(apptID), OK);
     }
 
-    @GetMapping("/doctor/{doctor_username}")
+
+    /**
+     * Creating a REST API: "GET http://localhost:8080/api/account/appointment/doctor/doctorView/{doctor_username}"
+     * Get all appointments associated with a specific doctor's username.
+     * A doctor's username needs to be passed in the URL as a path variable.
+     *
+     * In Postman, select GET as the method, input the URL, replacing {doctor_username} with a valid doctor's username.
+     * Authorization header should be set with a valid JWT token for a doctor account.
+     */
+    @GetMapping("/doctor/doctorView/{doctor_username}")
     public ResponseEntity<List<AppointmentEntity>> getAppointmentsByDoctorUsername(@PathVariable("doctor_username") String doctor_username) {
         return new ResponseEntity<>(appointmentService.getAppointmentsByDoctorUsername(doctor_username), OK);
     }
 
-    @GetMapping("/patient/{patient_username}")
+
+    /**
+     * Creating a REST API: "GET http://localhost:8080/api/account/appointment/patient/doctorView/{patient_username}"
+     * Get all appointments associated with a specific patient's username.
+     * A patient's username needs to be passed in the URL as a path variable.
+     *
+     * In Postman, select GET as the method, input the URL, replacing {patient_username} with a valid patient's username.
+     * Authorization header should be set with a valid JWT token for a patient account.
+     */
+    @GetMapping("/patient/doctorView/{patient_username}")
     public ResponseEntity<List<AppointmentEntity>> getAppointmentsByPatientUsername(@PathVariable("patient_username") String patient_username) {
         return new ResponseEntity<>(appointmentService.getAppointmentsByPatientUsername(patient_username), OK);
     }
 
-    @GetMapping("/{start_time}_{end_time}")
+
+    /**
+     * Creating a REST API: "GET http://localhost:8080/api/account/appointment/allView/{start_time}_{end_time}"
+     * Get appointments for the logged-in user (either patient or doctor) between a specific start and end time.
+     * The start_time and end_time (format: yyyy-MM-dd HH:mm:ss) need to be passed in the URL as path variables.
+     *
+     * In Postman, select GET as the method, input the URL, replacing {start_time} and {end_time} with valid timestamps.
+     * Authorization header should be set with a valid JWT token for a patient or a doctor account.
+     */
+    @GetMapping("/allView/{start_time}_{end_time}")
     public ResponseEntity<List<AppointmentEntity>> getAppointmentsBtwStartTimeAndEndDateTime(
+            @RequestHeader("Authorization") String authHeader,
             @PathVariable("start_time") Timestamp start_time,
             @PathVariable("end_time") Timestamp end_time) {
-        List<AppointmentEntity> appts = appointmentService.getAppointmentsBtwStartTimeAndEndTime(start_time, end_time);
+
+        String jwtToken = authHeader.substring(7);
+        String username = jwtService.extractUsername(jwtToken);
+        String type = accountService.getAccountTypeByUsername(username);
+
+        List<AppointmentEntity> appts;
+        if (type.equals("PATIENT")) {
+            appts = appointmentService.getAppointmentsByPatientUsernameBtwStartTimeAndEndTime(username, start_time, end_time);
+        } else {
+            appts = appointmentService.getAppointmentsByDoctorUsernameBtwStartTimeAndEndTime(username, start_time, end_time);
+        }
         return new ResponseEntity<>(appts, OK);
     }
 
-    @PutMapping("/update")
-    public ResponseEntity<AppointmentResponse> updateAppointment(@RequestBody AppointmentRequest request) throws AppointmentDateException, AppointmentIdNotFoundException {
+
+    /**
+     * Creating a REST API: "PUT http://localhost:8080/api/account/appointment/patient/patientView/update"
+     * Update an existing appointment for logged-in patients
+     * Passing in JSON (date should be in "yyyy-MM-dd HH:mm:ss"):
+     * {
+     *     "appointmentId": ...,
+     *     "appointmentTitle": ...,
+     *     "doctorName": ...,
+     *     "description": ...,
+     *     "startTime": ...,
+     *     "endTime": ...
+     * }
+     *
+     * In Postman, select PUT as the method, input the URL, and set the request body to raw JSON.
+     * Authorization header should be set with a valid JWT token for a patient account.
+     */
+    @PutMapping("/patient/patientView/update")
+    public ResponseEntity<AppointmentResponse> updateAppointment(
+            @RequestHeader("Authorization") String authHeader,
+            @RequestBody AppointmentRequest request
+    ) throws AppointmentIdNotFoundException, AppointmentDateException {
+        String jwtToken = authHeader.substring(7);
+        String username = this.jwtService.extractUsername(jwtToken);
+        request.setPatientName(username);
         return ResponseEntity.ok(appointmentService.updateAppointment(request));
     }
-    // Update an appointment
 
-    @GetMapping("/date/{date}")
+
+
+
+    /**
+     * Creating a REST API: "GET http://localhost:8080/api/account/appointment/doctor/doctorView/date/{date}"
+     * Get appointments for a specific date
+     * A date needs to be passed in the URL as a path variable (date should be in "yyyy-MM-dd").
+     *
+     * In Postman, select GET as the method, input the URL, replacing {date} with a valid date.
+     * Authorization header should be set with a valid JWT token for a doctor account.
+     */
+    @GetMapping("/doctor/doctorView/date/{date}")
     public ResponseEntity<List<AppointmentEntity>> getAppointmentsByDate(@PathVariable("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         return new ResponseEntity<>(appointmentService.getAppointmentsByDate(date), OK);
     }
-    // Get appointments for a specific date
 
-    @GetMapping("/count")
+
+    /**
+     * Creating a REST API: "GET http://localhost:8080/api/account/appointment/doctor/doctorView/count"
+     * Get total number of appointments for the logged-in patient.
+     *
+     * In Postman, select GET as the method, input the URL.
+     * Authorization header should be set with a valid JWT token for a doctor account.
+     */
+    @GetMapping("/doctor/doctorView/count")
     public ResponseEntity<Long> getTotalAppointments() {
         return new ResponseEntity<>(appointmentService.getTotalAppointments(), OK);
     }
-    // Get the total number of appointments
 }
--- src/main/java/com/clinic/appointment/clinicappointmentsystem/entity/appointment/AppointmentRepo.java
+++ src/main/java/com/clinic/appointment/clinicappointmentsystem/entity/appointment/AppointmentRepo.java
@@ -6,9 +6,15 @@ import java.sql.Timestamp;
 import java.util.List;
 
 public interface AppointmentRepo extends JpaRepository<AppointmentEntity, Integer> {
+    void deleteAppointmentEntitiesByDoctorUsername(String username);
+    void deleteAppointmentEntitiesByPatientUsername(String username);
     List<AppointmentEntity> findAppointmentEntitiesByStartDateAfterAndEndDateBefore(Timestamp startDate, Timestamp endDate);
 
+    List<AppointmentEntity> findAppointmentEntitiesByPatientUsernameAndStartDateAfterAndEndDateBefore(String username, Timestamp startDate, Timestamp endDate);
+    List<AppointmentEntity> findAppointmentEntitiesByDoctorUsernameAndStartDateAfterAndEndDateBefore(String username, Timestamp startDate, Timestamp endDate);
     List<AppointmentEntity> findAppointmentEntitiesByPatientUsername(String patientUsername);
 
     List<AppointmentEntity> findAppointmentEntitiesByDoctorUsername(String doctorUsername);
+
+    List<AppointmentEntity> findAppointmentEntitiesByDoctorUsernameAndEndDateAfterAndStartDateBefore(String doctorName, Timestamp startDate, Timestamp endDate);
 }
